84070ca17ba7baa202e8751bf060083f9b9a1aa3
jessi <jessi.nowski@hotmail.de>
2024-01-30 15:24:56 +0100

    add bookarray

--- src/main/java/org/github/mahambach/Book.java
+++ src/main/java/org/github/mahambach/Book.java
@@ -7,6 +7,12 @@ public class Book {
 
     private String isbn;
 
+    public Book(String title, String author, String isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn =isbn;
+    }
+
     public String getIsbn() {
         return isbn;
     }
--- src/main/java/org/github/mahambach/Main.java
+++ src/main/java/org/github/mahambach/Main.java
@@ -1,7 +1,12 @@
 package org.github.mahambach;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+    Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", "1234");
+    Book book2 = new Book("1984", "George Orwell", "5678");
+    Book book3= new Book("Die Kinder der Zeit", "Adrian Tschaikowsky", "5487");
+
+    Book[] bookarray= {book1, book2, book3};
+    Library library1 = new Library("Gute Bücher", bookarray);
+
 }
