cc7858d61ef1e3651ea94842f452b287f2ed95df
mahambach <ma.hambach@gmx.net>
2024-01-30 06:26:54 -0800

    add main

--- src/main/java/org/github/mahambach/Main.java
+++ src/main/java/org/github/mahambach/Main.java
@@ -1,12 +1,16 @@
 package org.github.mahambach;
 
+
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", "1234");
+        Book book2 = new Book("1984", "George Orwell", "5678");
+        Book book3= new Book("Die Kinder der Zeit", "Adrian Tschaikowsky", "5487");
 
-    Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", "1234");
-    Book book2 = new Book("1984", "George Orwell", "5678");
-    Book book3= new Book("Die Kinder der Zeit", "Adrian Tschaikowsky", "5487");
+        Book[] bookarray= {book1, book2, book3};
+        Library library1 = new Library("Gute Bücher", bookarray);
 
-    Book[] bookarray= {book1, book2, book3};
-    Library library1 = new Library("Gute Bücher", bookarray);
+    }
+
 
 }
